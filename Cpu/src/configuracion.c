56f15c9b495d69ea4a1457039f699edf1f859a8b
lukblan <luk.blanko@gmail.com>
2023-05-05 21:21:01 +0000

    Agregada funcion para leer archivo de configuracion

--- Cpu/src/configuracion.c
+++ Cpu/src/configuracion.c
@@ -5,6 +5,11 @@
 configuracion obtenerConfiguracion() {
   configuracion configuracion;
   t_config* archivoConfiguracion = config_create("cpu.config");
+  configuracion.IP_MEMORIA = string_duplicate(config_get_string_value(archivoConfiguracion, "IP_MEMORIA"));
+  configuracion.TAM_MAX_SEGMENTO = config_get_int_value(archivoConfiguracion, "TAM_MAX_SEGMENTO");
+  configuracion.PUERTO_ESCUCHA = config_get_int_value(archivoConfiguracion, "PUERTO_ESCUCHA");
+  configuracion.PUERTO_MEMORIA = config_get_int_value(archivoConfiguracion, "PUERTO_MEMORIA");
+  configuracion.RETARDO_INSTRUCCION = config_get_int_value(archivoConfiguracion, "RETARDO_INSTRUCCION");
   config_destroy(archivoConfiguracion);
   return configuracion;
 }
