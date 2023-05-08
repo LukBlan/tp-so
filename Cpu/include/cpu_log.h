7bbc23076ef758dbc6cf04e6b400b20c9af523ca
lukblan <luk.blanko@gmail.com>
2023-05-08 15:34:25 +0000

    Separado funcioens de log en un archivo aparte

---
+++ Cpu/include/cpu_log.h
@@ -0,0 +1,6 @@
+#ifndef CPU_LOG_H_
+#define CPU_LOG_H_
+
+t_log *iniciar_logger_cpu();
+
+#endif /* CPU_LOG_H_ */
---
+++ Cpu/src/cpu_log.c
@@ -0,0 +1,3 @@
+t_log *iniciar_logger_cpu() {
+  return log_create("CPU.log", "CPU", true, LOG_LEVEL_INFO);
+}
