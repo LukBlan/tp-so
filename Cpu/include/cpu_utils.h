c903cc8ed5de12826319d78dd1a4d2c2bee6cbce
lgmezabal <lgmezabal@frba.utn.edu.ar>
2023-05-11 20:53:54 +0000

    funcion set

--- Cpu/include/cpu_utils.h
+++ Cpu/include/cpu_utils.h
@@ -4,6 +4,34 @@
 #include <socket/cliente.h>
 #include <commons/string.h>
 #include <commons/txt.h>
+union Valor{
+	uint32_t cuatro_bytes;
+	uint64_t ocho_bytes;
+	uint32_t dieciseis_bytes [4];
+};
+struct Registro{
+	char nombre[20];
+	union Valor valor;
+};
+extern AX;
+extern BX;
+extern CX;
+extern DX;
 
+extern EAX;
+extern EBX;
+extern ECX;
+extern EDX;
+
+extern RAX;
+extern RBX;
+extern RCX;
+extern RDX;
+
+struct Registro buscar_registro (char nombre_registro);
+
+void setear_valor_registro(char nombre_registro,char valor);
+
+TipoInstruccion obtener_tipo_instruccion(char *instruccion);
 
 #endif /* CPU_UTILS_H_ */
--- Cpu/src/cpu_utils.c
+++ Cpu/src/cpu_utils.c
@@ -1,4 +1,78 @@
 #include <cpu_utils.h>
+struct Registro AX = {
+	.nombre = "AX",
+	.valor.cuatro_bytes=0};
+
+struct Registro BX = {
+	.nombre = "BX",
+	.valor.cuatro_bytes=0};
+
+struct Registro CX = {
+	.nombre = "CX",
+	.valor.cuatro_bytes=0};
+
+struct Registro DX = {
+	.nombre = "DX",
+	.valor.cuatro_bytes=0};
+
+struct Registro EAX = {
+	.nombre = "EAX",
+	.valor.ocho_bytes=0};
+
+struct Registro EBX = {
+	.nombre = "EBX",
+	.valor.ocho_bytes=0};
+
+struct Registro ECX = {
+	.nombre = "ECX",
+	.valor.ocho_bytes=0};
+
+struct Registro EDX = {
+	.nombre = "EDX",
+	.valor.ocho_bytes=0};
+
+struct Registro RAX = {
+	.nombre = "RAX",
+	.valor.dieciseis_bytes=0};
+
+struct Registro RBX = {
+	.nombre = "RBX",
+	.valor.dieciseis_bytes=0};
+
+struct Registro RCX = {
+	.nombre = "RCX",
+	.valor.dieciseis_bytes=0};
+
+struct Registro RDX = {
+	.nombre = "RDX",
+	.valor.dieciseis_bytes=0};
+
+struct Registro registros[]={
+		AX,
+		BX,
+		CX,
+		DX,
+		EAX,
+		EBX,
+		ECX,
+		EDX,
+		RAX,
+		RBX,
+		RCX,
+		RDX
+};
+
+struct Registro buscar_registro (char nombre_registro){
+	for(int i = 0; i<16;i++){
+		if (strcmp(registros[i].nombre,nombre_registro)==0)
+			return registros[i];
+	}
+	return 0;
+}
+void setear_valor_registro(char nombre_registro,char valor){
+	struct Registro registro_encontrado = buscar_registro(nombre_registro);
+	registro_encontrado.valor = valor;
+}
 
 TipoInstruccion obtener_tipo_instruccion(char *instruccion)
 {
@@ -27,10 +101,10 @@ void ejecutar_EXIT(Pcb *pcb, int socketKernel)
 }
 */
 
-void ejecutar_set() {
-
+/*void ejecutar_set(char nom_registro,char valor) {
+	setear_valor_registro(nom_registro,valor);
 }
-
+*/
 void ejecutar_yield() {
 
 }
