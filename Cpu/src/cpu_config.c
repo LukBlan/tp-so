d3a57fa4e8dce54cc83d253a000467125b8ff61d
lukblan <luk.blanko@gmail.com>
2023-05-17 22:05:37 +0000

    Ahora obtenerConfiguracion usa abrir_config de la shared

--- Cpu/src/cpu_config.c
+++ Cpu/src/cpu_config.c
@@ -1,10 +1,11 @@
 #include <cpu_config.h>
 #include <commons/config.h>
 #include <commons/string.h>
+#include <socket/cliente.h>
 
 configuracion* obtenerConfiguracion(char* pathArchivo) {
   configuracion* config = malloc(sizeof(configuracion));
-  t_config* fileConfig = config_create(pathArchivo);
+  t_config* fileConfig = abrir_config(pathArchivo);
   config->IP_MEMORIA = string_duplicate(config_get_string_value(fileConfig, "IP_MEMORIA"));
   config->IP_ESCUCHA = string_duplicate(config_get_string_value(fileConfig, "IP_ESCUCHA"));
   config->TAM_MAX_SEGMENTO = config_get_int_value(fileConfig, "TAM_MAX_SEGMENTO");
