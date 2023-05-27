6245caa4bfab57d186e292ea40a9139443036ead
lukblan <luk.blanko@gmail.com>
2023-05-27 14:19:38 +0000

    Kernel recive correctamente las instrucciones

--- Kernel/include/conexion_kernel.h
+++ Kernel/include/conexion_kernel.h
@@ -1,8 +1,10 @@
 #ifndef CONEXION_KERNEL_H_
 #define CONEXION_KERNEL_H_
 
+#include <commons/collections/list.h>
+
 void* montar_servidor(void*);
 void* conectar_con_memoria(void*);
 void* conectar_con_cpu(void*);
-void obtenerDatos(socketCliente);
+t_list* generarListaDeInstrucciones(int socketCliente);
 #endif /* CONEXION_KERNEL_H_ */
--- Kernel/src/conexion_kernel.c
+++ Kernel/src/conexion_kernel.c
@@ -11,33 +11,75 @@ void* montar_servidor(void* args) {
   while (1) {
     int socketCliente = esperar_cliente(socketServidor);
     obtener_codigo_operacion(socketCliente);
-    obtenerDatos(socketCliente);
+    generarListaDeInstrucciones(socketCliente);
     close(socketCliente);
   }
   close(socketServidor);
 }
 
-
-void obtenerDatos(socketCliente) {
+buffer* obtenerBuffer(int socketCliente) {
   int tamanioBuffer;
   recv(socketCliente, &tamanioBuffer, sizeof(int), MSG_WAITALL);
-  printf("%d\n", tamanioBuffer);
-
   void* stream = malloc(tamanioBuffer);
   recv(socketCliente, stream, tamanioBuffer, MSG_WAITALL);
   buffer* buffer = malloc(tamanioBuffer + sizeof(int));
   buffer->stream = stream;
   buffer->size = tamanioBuffer;
+  return buffer;
+}
 
+t_list* generarListaDeInstrucciones(int socketCliente) {
+  t_list* instrucciones = list_create();
+  buffer* buffer = obtenerBuffer(socketCliente);
   int posicion = 0;
-  int tamanioPrimeraInstruccion;
-  memcpy(&tamanioPrimeraInstruccion, stream, sizeof(int));
-  posicion += sizeof(int);
-  printf("%d\n", tamanioPrimeraInstruccion);
 
-  char* nombreInstruccion = string_new();
-  memcpy(nombreInstruccion, stream + posicion, tamanioPrimeraInstruccion);
-  printf("%s\n", nombreInstruccion);
+  int cantidadDeInstrucciones;
+
+  memcpy(&cantidadDeInstrucciones, buffer->stream, sizeof(int));
+  posicion += sizeof(int);
+  printf("cantidad instrucciones %d\n", cantidadDeInstrucciones);
+
+  for (int i = 0; i < cantidadDeInstrucciones; i++) {
+    int tamanioPrimeraInstruccion;
+    memcpy(&tamanioPrimeraInstruccion, buffer->stream + posicion, sizeof(int));
+    posicion += sizeof(int);
+
+    char* nombreInstruccion = string_new();
+    memcpy(nombreInstruccion, buffer->stream + posicion, tamanioPrimeraInstruccion);
+    posicion += tamanioPrimeraInstruccion;
+    printf("%s ", nombreInstruccion);
+
+    int tamañoPrimerParametro;
+    memcpy(&tamañoPrimerParametro, buffer->stream + posicion, sizeof(int));
+    posicion += sizeof(int);
+
+    char* nombrePrimerParametro = string_new();
+    memcpy(nombrePrimerParametro, buffer->stream + posicion, tamañoPrimerParametro);
+    printf("%s ", nombrePrimerParametro);
+    posicion += tamañoPrimerParametro;
+
+    int tamañoSegundoParametro;
+    memcpy(&tamañoSegundoParametro, buffer->stream + posicion, sizeof(int));
+    posicion += sizeof(int);
+
+    char* nombreSegundoParametro = string_new();
+    memcpy(nombreSegundoParametro, buffer->stream + posicion, tamañoSegundoParametro);
+    printf("%s ", nombreSegundoParametro);
+    posicion += tamañoSegundoParametro;
+
+    int tamañoTercerParametro;
+    memcpy(&tamañoTercerParametro, buffer->stream + posicion, sizeof(int));
+    posicion += sizeof(int);
+
+    char* nombreTercerParametro = string_new();
+    memcpy(nombreTercerParametro, buffer->stream + posicion, tamañoTercerParametro);
+    printf("%s\n", nombreTercerParametro);
+    posicion += tamañoTercerParametro;
+    //printf("%s %s\n", nombreInstruccion, nombrePrimerParametro);
+
+  }
+
+  return instrucciones;
 }
 
 /*
