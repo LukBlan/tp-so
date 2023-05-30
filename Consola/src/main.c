3e328f357763c0d0d8071627e8ad5fe8c6a6fce6
lukblan <luk.blanko@gmail.com>
2023-05-30 15:35:44 +0000

    Algunos tabs y elmino saltos de linea

--- Consola/src/main.c
+++ Consola/src/main.c
@@ -4,7 +4,7 @@
 #include <utils.h>
 #include <instrucciones/lista_instrucciones.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   t_paquete* paquete;
   t_list* instrucciones;
 
