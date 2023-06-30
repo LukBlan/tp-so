189c82f30e973bd9afe8d1dc3615b46318702a8e
joselqm <joselquisbertmendoza@frba.utn.edu.ar>
2023-06-30 00:05:45 +0000

    borro lineas comentadas.

--- Kernel/src/main.c
+++ Kernel/src/main.c
@@ -6,19 +6,6 @@
 #include <estructuras.h>
 
 int main(int argc, char* argv[]) {
-
-	//t_recurso* recurso;
-
-  //espero a que me pidan un recurso
-
-  //int auxExisteRecurso = validoExistenciaDeRecurso(listaDeRecursos/*,recursopedido*/);
-
- // queue_clean(listaDeRecursos);
-
-  /*if ( cantRecursos > 0){
-	  queue_push(listaRecursos, 4);
-  }
-  */
   validarCantidadArgumentosMain(argc, 2);
   crearRecursos();
   cargarLogger("kernel.log");
