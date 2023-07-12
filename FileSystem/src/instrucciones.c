77d2e5ed9f52febca55cb6941e9f269a200d488c
lgmezabal <lgmezabal@frba.utn.edu.ar>
2023-07-12 12:01:06 -0300

    avanzo Fopen

--- FileSystem/src/instrucciones.c
+++ FileSystem/src/instrucciones.c
@@ -16,5 +16,25 @@
         fseek(arch->punteroArchivo, posicion, SEEK_SET);
     }
 
+    FILE* fcreate(char* nomArchivo){
+        fcbPath = string_new();
+        string_append(fcbPath,recursosFileSystem->configuracion->PATH_BLOQUES);
+        FILE* FCBdescriptor = fopen (filePath,"rb");
+        
+        if (descriptor != NULL) {
+			fclose(metadata_fd);
+				log_info(recursosFileSystem->logger, "FCB encontrada");
+			return;
+		}
+        log_info(recursosFileSystem->logger, "FCB no encontrado");
+        FCB* fcbArchivo = malloc(sizeof(FCB));
+        config_set_value(fcbArchivo, "nombre_archivo", nomArchivo);
+		config_set_value(fcbArchivo, "file_size", "0");
+		config_set_value(fcbArchivo, "punteroDirecto", ""); //TODO Ver punteros
+		config_set_value(fcbArchivo, "punteroIndirecto", "");
+        config_save(fcbArchivo);
+
+    }
+
 
 */
