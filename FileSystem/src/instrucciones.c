2addccc7b754b6cfd3450dcd5afce0da7faac3e6
lgmezabal <lgmezabal@frba.utn.edu.ar>
2023-07-14 12:25:48 +0000

    ahora compila

--- FileSystem/src/instrucciones.c
+++ FileSystem/src/instrucciones.c
@@ -1,35 +1,36 @@
 #include <commons/string.h>
+#include <commons/config.h>
 #include <conexiones.h>
 #include <fileSystem_conexiones.h>
 #include <recursos.h>
 
     archivoAbierto* agregarAArchivo(FILE* fd,char* nomArchivo){
-        archivoAbierto* archivo = malloc(sizeod(archivoAbierto));
+        archivoAbierto* archivo = malloc(sizeof(archivoAbierto));
         archivo -> nombre = nomArchivo;
         archivo -> punteroArchivo = fd;
         return archivo;
     }
-    contextoEjecucion* fcreate(char* nomArchivo, contextoEjecucucion* contexto){
-        fcbPath = string_new();
+    contextoEjecucion* fcreate(char* nomArchivo, contextoEjecucion* contexto){
+        char* fcbPath = string_new();
         string_append(fcbPath,recursosFileSystem->configuracion->PATH_BLOQUES);
         string_append(fcbPath,nomArchivo);
         FILE* FCBdescriptor = fopen (fcbPath,"rb");
         FILE* fileDescriptor = fopen("nomArchivo","rb");
         archivoAbierto*  arch = agregarAArchivo(fileDescriptor,nomArchivo);
         list_add(contexto -> archivosAbiertos,arch);
-        if (descriptor != NULL) {
-			fclose(metadata_fd);
+        if (FCBdescriptor != NULL) {
+			fclose(FCBdescriptor);
 				log_info(recursosFileSystem->logger, "FCB encontrada");
-			return;
+			// return;
 		}
         log_info(recursosFileSystem->logger, "FCB no encontrado");
         FCB* fcbArchivo = malloc(sizeof(FCB));
-        int ocuparBloques
-        config_set_value(fcbArchivo, "nombre_archivo", nomArchivo);
+        //int ocuparBloques;
+        /*config_set_value(fcbArchivo, "nombre_archivo", nomArchivo);
 		config_set_value(fcbArchivo, "file_size", "0");
 		config_set_value(fcbArchivo, "punteroDirecto", ""); //TODO Ver punteros
 		config_set_value(fcbArchivo, "punteroIndirecto", "");
-        config_save(fcbArchivo);
-
+        config_save(fcbArchivo);*/
+        return contexto;
     }
 
