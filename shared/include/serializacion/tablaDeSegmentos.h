d5db40ef7a902e1e6acf83c161eb3f2cacf363c3
lukblan <luk.blanko@gmail.com>
2023-07-18 00:23:51 +0000

    Agregada funcion para obtener tamanio bytes de tabla segmentos

---
+++ shared/include/serializacion/tablaDeSegmentos.h
@@ -0,0 +1,7 @@
+#ifndef TABLA_DE_SEGMENTOS_H
+#define TABLA_DE_SEGMENTOS_H
+
+  void serializarTablaDeSegmentos(t_buffer* buffer, t_list* tablaDeSegmentos);
+  int tamanioBytesTablaDeSegmentos(t_list* tablaDeSegmentos);
+
+#endif
