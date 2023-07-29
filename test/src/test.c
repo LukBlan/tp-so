3fe5e2e62719c61004d31a24d0d8a40c19e97d67
lukblan <luk.blanko@gmail.com>
2023-07-29 05:49:08 +0000

    Borro carpeta test

--- test/src/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <utils.h>
-
-int main(void) {
-  int cantidadBytes = bitsToBytes(8);
-	printf("numero %d\n", cantidadBytes);
-	return EXIT_SUCCESS;
-}
